27a222befaad1ec6c9dfc8a1e78958eb8d18bbde
DLrh <123506780+DLrh@users.noreply.github.com>
2023-02-15 10:04:09 -0800

    updates

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          React app updates via Github via OpenShift on AWS!!!! 
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
